d0722e643bb2e961949aa1e7d1eb731d25bfcac1
dyy1540984562 <1540984562@qq.com>
2020-02-21 18:21:19 +0800

    添加项目文件。

---
+++ show.cpp
@@ -0,0 +1,20 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat img = imread("C:\\opencv_picture_test\\test1.jpg");
+	if (img.empty())
+	{
+		printf("Could not find the image!\n");
+		return -1;
+	}
+
+	imshow("ImputImage", img);
+
+	waitKey(0);
+	return 0;
+}
