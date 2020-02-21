57ac46557917fba15259aa358fd2d7f9d16f30e1
dyy1540984562 <1540984562@qq.com>
2020-02-21 17:47:29 +0800

    添加项目文件。

---
+++ PrintNumber.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int check(int n)
+{
+	int result = 0;
+	if (n % 2 == 0)
+	{
+		result = 1;
+	}
+	else
+	{
+		result = 0;
+	}
+	return result;
+}
+int check_prime(int n)
+{
+	int result = 0;
+	if (n == 1 || n == 0) result = 1;
+	else
+	{
+		for (int i = 2;i <= sqrt(n);i++)
+		{
+			if (n % i == 0)
+			{
+				result = 0;
+				break;
+			}
+			else
+			{
+				result = 1;
+			}
+		}
+	}
+	return result;
+}
+int main()
+{
+	int totalnumber = 100;
+	int result = 0;
+	for (int i = 0;i < totalnumber;i++)
+	{
+		result = check_prime(i);
+		if (result == 1)
+		{
+			cout << "the number is:" << i << endl;
+		}
+	}
+	system("pause");
+	return 0;
+}
