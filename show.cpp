8d9040c07cec5cdc61739ccbdcce1e00b1212029
1540984562 <1540984562@qq.com>
2020-05-22 14:20:29 +0800

    练习1 练习2

--- show.cpp
+++ show.cpp
@@ -6,117 +6,168 @@
 #include <stdio.h>
 #include <time.h>
 #include <math.h>  
-#include "My_ImageProssing_base.h"
+//#include "My_ImageProssing_base.h"
 #include<opencv2/imgproc/types_c.h> 
 #define WINDOW_NAME "【程序窗口】"			
 using namespace cv;
 using namespace std;
 
 //RNG g_rng(12345);
-
-
-
-/*********************************************练习1：gama矫正*****************************************************/
+/****练习1：背景差分**********/
 //int main()
 //{
-//	Mat srcImage = imread("D:\\opencv_picture_test\\直方图均衡与矫正\\face.jpg",1);			//原图
-//	if (srcImage.empty())
+//	//实例化的同时初始化
+//	VideoCapture capture("D:\\opencv_picture_test\\videos\\video1.avi");		//类似于 int a=1;
+//	Mat frame;
+//	Mat bgMat, subMat, bny_subMat;	//存储每一帧的图像
+//	int cnt = 0;
+//	while (1)
 //	{
-//		printf("Could not find the image!\n");
-//		return -1;
-//	}
-//	//【转化为灰度图】
-//	cvtColor(srcImage, srcImage,COLOR_BGR2GRAY);
-//	int row_num = srcImage.rows;
-//	int col_num = srcImage.cols;
-//	//进行表的建立
-//	float gama = 0.3;
-//	float LUT[256] = { 0 };
-//	for (int i = 0; i < 256; i++) 
-//	{
-//		LUT[i] = pow(i * 1.0 / 255, gama);
-//	}
-//	for (int i = 0;i < row_num;i++)	//行循环
-//	{
-//		for (int j = 0;j < col_num;j++)	//列循环
-//		{
-//			srcImage.at<uchar>(i, j) = LUT[srcImage.at<uchar>(i, j)] * 255;
+//		capture >> frame;
+//		cvtColor(frame, frame, COLOR_BGR2GRAY);
+//		if (cnt == 0) {
+//			//第一帧，获得背景图像
+//			frame.copyTo(bgMat);
 //		}
+//		else {
+//			//第二帧开始背景差分
+//			//背 景图像和当前图像相减
+//			absdiff(frame, bgMat, subMat);
+//			//差分结果二值化
+//			threshold(subMat, bny_subMat, 50,255,CV_THRESH_BINARY);
+//			imshow(" b_subMat" , bny_subMat);
+//			imshow(" subMat", subMat);
+//			waitKey(30);
+//		}
+//		cnt++;
 //	}
-//
-//	//【显示结果】
-//	namedWindow("gama", WINDOW_NORMAL);//WINDOW_NORMAL允许用户自由伸缩窗口
-//	imshow("gama", srcImage);
-//	waitKey(0);
 //	return 0;
 //}
 
 
-/*********************************************练习2：直方图均衡*****************************************************/
-//int main()
-//{
-//	Mat srcImage = imread("D:\\opencv_picture_test\\直方图均衡与矫正\\etest.jpg",1),dstImage;			//原图
-//	if (srcImage.empty())
-//	{
-//		printf("Could not find the image!\n");
-//		return -1;
-//	}
-//	首先分到三个通道里面去
-//	Mat channels[3];
-//	Mat dst_channels[3];
-//	src为要分离的Mat对象  
-//	split(srcImage, channels);               //利用vector对象分离  
-//
-//	imshow("B", channels[0]);
-//	imshow("G", channels[1]);
-//	imshow("R", channels[2]);
-//	对每个通道的图像进行均衡化
-//	for (int i = 0;i <= 2;i++)
-//	{
-//		equalizeHist(channels[i], dst_channels[i]);
-//	}
-//	合并
-//	 merge(dst_channels,3, dstImage);
-//	【显示结果】
-//	namedWindow("经过直方图均衡化后的图", WINDOW_NORMAL);//WINDOW_NORMAL允许用户自由伸缩窗口
-//	imshow("经过直方图均衡化后的图", dstImage);
-//	waitKey(0);
-//	return 0;
-//}
-
-
-/*********************************************练习3：gama矫正*****************************************************/
-int main()
+/****练习2：实现高斯建模与背景差分**********/
+int calcGaussianBackground(std::vector<cv::Mat> srcMats, cv::Mat& meanMat, cv::Mat& varMat)
 {
-	Mat srcImage = imread("D:\\opencv_picture_test\\直方图均衡与矫正\\gtest.jpg",1);			//原图
-	if (srcImage.empty())
+
+	int rows = srcMats[0].rows;
+	int cols = srcMats[0].cols;
+
+
+	for (int h = 0; h < rows; h++)
 	{
-		printf("Could not find the image!\n");
-		return -1;
-	}
-	//【转化为灰度图】
-	cvtColor(srcImage, srcImage,COLOR_BGR2GRAY);
-	int row_num = srcImage.rows;
-	int col_num = srcImage.cols;
-	//进行表的建立
-	float gama = 0.3;
-	float LUT[256] = { 0 };
-	for (int i = 0; i < 256; i++) 
-	{
-		LUT[i] = pow(i * 1.0 / 255, gama);
-	}
-	for (int i = 0;i < row_num;i++)	//行循环
-	{
-		for (int j = 0;j < col_num;j++)	//列循环
+		for (int w = 0; w < cols; w++)
 		{
-			srcImage.at<uchar>(i, j) = LUT[srcImage.at<uchar>(i, j)] * 255;
+
+			int sum = 0;
+			float var = 0;
+			//求均值
+			for (int i = 0; i < srcMats.size(); i++) {
+				sum += srcMats[i].at<uchar>(h, w);
+			}
+			meanMat.at<uchar>(h, w) = sum / srcMats.size();
+			//求方差
+			for (int i = 0; i < srcMats.size(); i++) {
+				var += pow((srcMats[i].at<uchar>(h, w) - meanMat.at<uchar>(h, w)), 2);
+			}
+			varMat.at<float>(h, w) = var / srcMats.size();
 		}
 	}
 
-	//【显示结果】
-	namedWindow("gama", WINDOW_NORMAL);//WINDOW_NORMAL允许用户自由伸缩窗口
-	imshow("gama", srcImage);
-	waitKey(0);
 	return 0;
 }
 
+int gaussianThreshold(cv::Mat srcMat, cv::Mat meanMat, cv::Mat varMat, float weight, cv::Mat& dstMat)
+{
+	int srcI;
+	int meanI;
+	int dstI;
+	int rows = srcMat.rows;
+	int cols = srcMat.cols;
+
+	for (int h = 0; h < rows; h++)
+	{
+		for (int w = 0; w < cols; w++)
+		{
+			srcI = srcMat.at<uchar>(h, w);
+			meanI = meanMat.at<uchar>(h, w);
+			int dif = abs(srcI - meanI);
+			int th = weight * varMat.at<float>(h, w);
+
+			if (dif > th) {
+
+				dstMat.at<uchar>(h, w) = 255;
+			}
+			else {
+				dstMat.at<uchar>(h, w) = 0;
+			}
+		}
+	}
+
+	return 0;
+}
+
+int main()
+{
+	//实例化的同时初始化
+	//VideoCapture capture("D:\\opencv_picture_test\\videos\\video1.avi");		//类似于 int a=1;
+	//----------------------读取视频文件--------------------------
+	VideoCapture capVideo(0);
+	//VideoCapture capVideo("../testImages\\vtest.avi");
+
+	//如果视频打开失败
+	if (!capVideo.isOpened()) {
+		std::cout << "Unable to open video!" << std::endl;
+		return -1;
+	}
+
+	//用来计算背景模型的图像
+	std::vector<cv::Mat> srcMats;
+
+
+	//参数设置
+	int nBg = 200;		//用来建立背景模型的数量
+	float wVar = 1.25;		//方差权重
+
+	int cnt = 0;
+	cv::Mat frame;
+	cv::Mat meanMat;
+	cv::Mat varMat;
+	cv::Mat dstMat;
+
+	while (true)
+	{
+		capVideo >> frame;
+		cvtColor(frame, frame, COLOR_BGR2GRAY);
+
+		//前面的nBg帧，计算背景
+		if (cnt < nBg) {
+
+			srcMats.push_back(frame);
+
+			if (cnt == 0) {
+				std::cout << "reading frame " << std::endl;
+			}
+
+		}
+		else if (cnt == nBg) {
+			//计算模型
+			meanMat.create(frame.size(), CV_8UC1);
+			varMat.create(frame.size(), CV_32FC1);
+			std::cout << "calculating background models" << std::endl;
+			calcGaussianBackground(srcMats, meanMat, varMat);
+		}
+		else {
+			//背景差分
+			dstMat.create(frame.size(), CV_8UC1);
+			gaussianThreshold(frame, meanMat, varMat, wVar, dstMat);
+			imshow("result", dstMat);
+			imshow("frame", frame);
+			waitKey(30);
+		}
+
+		cnt++;
+
+	}
+
+	return 0;
+}
